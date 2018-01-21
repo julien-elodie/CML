a0965f45ca231c1b17b2a4f1ad750a0b7bb99708
JulienElodie <philous@JulienElodies-MacBook-Air.local>
2018-01-22 00:10:23 +0800

    init project

---
+++ cmake-build-debug/CMakeFiles/CML.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CML.dir/main.cpp.o"
+  "CML.pdb"
+  "CML"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CML.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
